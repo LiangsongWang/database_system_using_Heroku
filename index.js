2d6b03ed6e2220b06a3522c713e1da5ccb8b112f
liangsong wang <lwa115@sfu.ca>
2020-06-18 15:44:17 -0700

    database changer

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-const { Pool } = require('pg');
+const {
+  Pool
+} = require('pg');
 var pool;
 pool = new Pool({
   // connectionString: 'postgres://postgres:wlsdean@localhost/users'
@@ -12,33 +14,115 @@ pool = new Pool({
 var app = express()
 
 app.use(express.json());
-app.use(express.urlencoded({extend:false}));
+app.use(express.urlencoded({
+  extend: false
+}));
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => res.render('pages/index'));
-app.get('/database',(req,res) => {
-  var getUsersQuery = `SELECT * FROM usr`;
-  pool.query(getUsersQuery,(error,result)=>{
-    if (error){
+app.get('/home', (req, res) => res.render('pages/home'));
+app.get('/add', (req, res) => res.render('pages/add'));
+app.get('/del', (req, res) => res.render('pages/del'));
+app.get('/upd', (req, res) => res.render('pages/upd'));
+app.get('/database', (req, res) => {
+  var getUsersQuery = `SELECT * FROM Person`;
+  pool.query(getUsersQuery, (error, result) => {
+    if (error) {
       res.end(error);
     }
-    var results = {'rows':result.rows}
-    res.render('pages/db',results);
+    var results = {
+      'rows': result.rows
+    }
+    res.render('pages/db', results);
   })
 
 });
-app.post('/adduser',(req,res) => {
-  console.log("psot request for /adduser");
-  var uname = req.body.uname;
-  var age = req.body.age;
-  res.send(`username: ${ uname },age: ${ age }!!!`);
+
+app.get('/mainpage', (req, res) => {
+  var getUsersQuery = `SELECT * FROM Person`;
+  pool.query(getUsersQuery, (error, result) => {
+    if (error) {
+      res.end(error);
+    }
+    var results = {
+      'rows': result.rows
+    }
+    res.render('pages/mp', results);
+  })
+
 });
-app.get('/users/:id',(req,res) =>{
-  var uid = req.params.id;
-  console.log(req.params.id);
-  res.send("got it!");
+
+
+app.post('/add_person', (req, res) => {
+  console.log("post request for /add_person");
+  var name = req.body.name;
+  var size = req.body.size;
+  var height = req.body.height;
+  var type = req.body.cType;
+  res.send(`insert a person name: ${ name },size: ${ size },height: ${height},type: ${type}`);
+  pool.query(
+    `INSERT INTO Person VALUES('${ name }', ${ size }, ${ height }, '${ type }')`,
+    (err, res) => {
+      console.log(err, res);
+    }
+  );
+
 });
+
+
+
+app.post('/del_person', (req, res) => {
+  console.log("post request for /add_person");
+  var name = req.body.name;
+  res.send(`delete a person  name: ${ name }`);
+  pool.query(
+    `DELETE FROM Person WHERE name='${ name }'`,
+    (err, res) => {
+      console.log(err, res);
+    }
+
+  );
+
+});
+
+
+app.post('/upd_person', (req, res) => {
+  console.log("post request for /add_person");
+  var name = req.body.name;
+  var size = req.body.size;
+  var height = req.body.height;
+  var type = req.body.cType;
+  res.send(`update ${ name } \nsize: ${ size },height: ${height},type: ${type}`);
+  pool.query(
+    `UPDATE Person  SET size =${ size }, height = ${ height },type = '${ type }' WHERE name ='${ name }'`,
+    (err, res) => {
+      console.log(err, res);
+    }
+  );
+
+});
+
+
+
+app.get('/person/:name', (req, res) => {
+  var name = req.params.name;
+  console.log(req.params.name);
+  var getUsersQuery = `SELECT * FROM Person WHERE name = '${name}'`;
+  pool.query(getUsersQuery, (error, result) => {
+    if (error) {
+      res.end(error);
+    }
+    var results = {
+      'rows': result.rows
+    }
+    res.render('pages/db', results);
+  })
+});
+
+
+
+
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
